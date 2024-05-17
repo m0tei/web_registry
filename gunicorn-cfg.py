05875188f42b5beb300231a74def521fa67b0c20
m0tei <david.matei.md@gmail.com>
2024-05-18 02:01:09 +0300

    good

--- gunicorn-cfg.py
+++ gunicorn-cfg.py
@@ -4,9 +4,9 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 bind = '0.0.0.0:5000'
-workers = 3
+workers = 1
+worker_class = 'eventlet'
 accesslog = '-'
 loglevel = 'debug'
 capture_output = True
 enable_stdio_inheritance = True
-
